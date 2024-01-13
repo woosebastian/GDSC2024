e4fa84451fa8543204e31edc686eff08126cfdce
Sebastian W <woosebastian@gmail.com>
2024-01-13 12:07:51 -0800

    set up files and working through a tutorial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  const name = 'John';
+  const isNameShowing = false;
+
+  return (
+    <div className="App">
+      <h1>Hello, {isNameShowing ? name : 'someone'}!</h1>
+    </div>
+  );
+}
+
+export default App;
