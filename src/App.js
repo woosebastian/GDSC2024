53116edf8729083d30d5680caebe8a0b02b12877
Sebastian W <woosebastian@gmail.com>
2024-01-27 16:26:46 -0800

    Co-authored-by: ananyabalagere <ananyabalagere@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
 // https://www.youtube.com/watch?v=b9eMGE7QtTk
 
-import { useState, useEffect } from 'react';
-
-import MovieCard from './MovieCard';
+import { useEffect } from 'react';
 
 import './App.css';
-import SearchIcon from './search.svg';
-
-// 20073185
-
-const API_URL = 'http://www.omdbapi.com?apikey=20073185';
-
-const movie1 = {
-    "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
-    "Year": "2007",
-    "imdbID": "tt1132238",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
-}
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-
-        setMovies(data.Search);
-    }
-
     useEffect(() => {
-        searchMovies('Spiderman');
+
     }, []);
 
     return (
         <div className="app">
-            <h1>MovieLand</h1>
-
-            <div className="search">
-                <input
-                    placeholder="Search for movies"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <img
-                    src={SearchIcon}
-                    alt="search"
-                    onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
-
-            {movies?.length > 0
-                ? (
-                    <div className="container">
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie} />
-                        ))}
-                    </div>
-                ) : (
-                    <div className="empty">
-                        <h2>No movies found</h2>
-                    </div>
-                )}
+            <h1 className="title">Course Planner</h1>
+            <a className="signInButton" href="https://www.google.com/">
+                <div className="logIn">
+                    {/* https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Creating_hyperlinks */}
+                    {/* <button className="signInButton"> */}
+                    Sign in with Google
+                    {/* </button> */}
+                </div>
+            </a>
         </div>
     );
 }
