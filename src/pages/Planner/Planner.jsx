fb71913ed95bc4f4f03a0998f27e5347dece07c1
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-02-25 10:22:42 -0800

    it loads at least

--- src/pages/Planner/Planner.jsx
+++ src/pages/Planner/Planner.jsx
@@ -28,11 +28,79 @@ import {
 import './Planner.css';
 import '../../MajorContext';
 
+// https://dev.to/jeetvora331/different-types-of-export-in-react-21p8
+export const generateSchedule = async (
+	currentUser,
+	navigate,
+	setUserSchedule
+) => {
+	// Add setUserSchedule as a parameter
+	try {
+		if (!currentUser) {
+			console.error('User not logged in');
+			navigate('/onboarding');
+			return;
+		}
+		console.log('generating schedule');
+		const db = getFirestore();
+
+		const majorReq = collection(db, 'majorRequirements');
+		const majorReqQuery = query(
+			majorReq,
+			where('major', '==', 'Computer Science B.S.')
+		);
+
+		const majorRequirementsDoc = await getDocs(majorReqQuery);
+
+		if (majorRequirementsDoc.empty) {
+			console.error('Major requirements document not found');
+			return;
+		}
+
+		const majorRequirements = majorRequirementsDoc.docs[0].data();
+
+		console.log('Major Requirements:', majorRequirements);
+
+		const schedulesCollectionRef = collection(db, 'schedules');
+		const schedulesQuery = query(
+			schedulesCollectionRef,
+			where('_userId', '==', currentUser.uid)
+		);
+
+		const schedulesQuerySnapshot = await getDocs(schedulesQuery);
+
+		if (!schedulesQuerySnapshot.empty) {
+			let newSchedule;
+			const scheduleData = schedulesQuerySnapshot.docs[0].data();
+			const currentSchedule = Array.isArray(scheduleData.schedule)
+				? scheduleData.schedule
+				: [];
+			const addedClasses = currentSchedule.flatMap((season) =>
+				season.flatMap((course) => course)
+			);
+			console.log('addedClasses: ', addedClasses);
+			newSchedule = generateNewSchedule(
+				scheduleData,
+				majorRequirements,
+				addedClasses
+			);
+			await updateScheduleInFirebase(currentUser.uid, newSchedule);
+			setUserSchedule(newSchedule); // Update userSchedule state with the new schedule
+		} else {
+			const newSchedule = generateNewSchedule({}, majorRequirements, {});
+			await updateScheduleInFirebase(currentUser.uid, newSchedule);
+			setUserSchedule(newSchedule); // Update userSchedule state with the new schedule
+		}
+	} catch (error) {
+		console.error('Error generating schedule:', error.message);
+	}
+};
+
 // https://stackoverflow.com/questions/57373072/state-is-not-defined
 const Planner = () => {
 	const [rerender, setRerender] = useState(false);
+	const { userSchedule, setUserSchedule } = useUserSchedule();
 	const [userName, setUserName] = useState('');
-	const [userSchedule, setUserSchedule] = useState([]);
 	const { currentUser } = useAuth();
 	const navigate = useNavigate();
 
@@ -70,7 +138,10 @@ const Planner = () => {
 							console.error('Schedule document does not exist.');
 						}
 					} else {
+						// Custom hook for managing user schedule state
+
 						// User document does not exist, open major popup
+						//** SET USER SCHEDULE */
 						navigate('/onboarding');
 					}
 				} catch (error) {
@@ -80,7 +151,7 @@ const Planner = () => {
 		};
 
 		fetchUserData();
-	}, [currentUser, navigate]);
+	}, [currentUser, navigate, setUserSchedule]);
 
 	console.log('userSchedule:', userSchedule);
 
@@ -95,219 +166,8 @@ const Planner = () => {
 		}
 	};
 
-	const updateScheduleInFirebase = async (userId, newSchedule) => {
-		try {
-			const db = getFirestore();
-
-			// Reference to the user's schedule document
-			const userScheduleDocRef = doc(db, 'schedules', userId);
-
-			// Use setDoc to either update the existing document or create a new one if it doesn't exist
-			await setDoc(userScheduleDocRef, {
-				_userId: userId,
-				schedule: newSchedule,
-			});
-
-			console.log('Schedule updated in Firebase');
-		} catch (error) {
-			console.error('Error updating schedule in Firebase:', error.message);
-		}
-	};
-
-	// https://dev.to/jeetvora331/different-types-of-export-in-react-21p8
-	const generateSchedule = async () => {
-		try {
-			if (!currentUser) {
-				console.error('User not logged in');
-				navigate('/onboarding');
-			}
-			console.log('generating schedule');
-			const db = getFirestore();
-
-			const majorReq = collection(db, 'majorRequirements');
-			const majorReqQuery = query(
-				majorReq,
-				where('major', '==', 'Computer Science B.S.')
-			);
-
-			const majorRequirementsDoc = await getDocs(majorReqQuery);
-
-			if (majorRequirementsDoc.empty) {
-				console.error('Major requirements document not found');
-				return;
-			}
-
-			const majorRequirements = majorRequirementsDoc.docs[0].data();
-
-			console.log('Major Requirements:', majorRequirements);
-
-			// Check if the user already has a schedule document
-			const schedulesCollectionRef = collection(db, 'schedules');
-			const schedulesQuery = query(
-				schedulesCollectionRef,
-				where('_userId', '==', currentUser.uid)
-			);
-
-			const schedulesQuerySnapshot = await getDocs(schedulesQuery);
-
-			let newSchedule;
-			const scheduleData = schedulesQuerySnapshot.docs[0].data();
-			const currentSchedule = Array.isArray(scheduleData.schedule)
-				? scheduleData.schedule
-				: [];
-			if (!schedulesQuerySnapshot.empty) {
-				// User already has a schedule document, generate a new schedule based on existing data
-				const addedClasses = currentSchedule.flatMap((season) =>
-					season.flatMap((course) => course)
-				);
-				console.log('addedClasses: ', addedClasses);
-				newSchedule = generateNewSchedule(
-					scheduleData,
-					majorRequirements,
-					addedClasses
-				);
-			} else {
-				// User doesn't have a schedule document, generate a new schedule from scratch
-				newSchedule = generateNewSchedule(scheduleData, majorRequirements, {});
-			}
-
-			// Update the schedule in the Firebase database
-			await updateScheduleInFirebase(currentUser.uid, newSchedule);
-
-			// Update userSchedule state with the new schedule
-			setUserSchedule(newSchedule);
-		} catch (error) {
-			console.error('Error generating schedule:', error.message);
-		}
-	};
-
 	// Function to generate a new schedule based on major requirements and pre-requisites
 	// Function to generate a new schedule based on major requirements and pre-requisites
-	const generateNewSchedule = (
-		previousSchedule,
-		majorRequirements,
-		addedClasses
-	) => {
-		const newSchedule = {};
-		const years = Array.from({ length: 4 }, (_, i) => i + 1);
-		const quarters = ['fall', 'winter', 'spring', 'summer'];
-		// const allClasses = []; // New array to keep track of all classes
-		years.forEach((year) => {
-			newSchedule[year] = {};
-			quarters.forEach((quarter) => {
-				const quarterSchedule = [];
-				console.log('in generate new schedule.');
-
-				// First, add classes with null value
-				Object.keys(majorRequirements.classRequirements).forEach((course) => {
-					if (
-						quarterSchedule.length < 2 &&
-						!addedClasses.includes(course) &&
-						majorRequirements.classRequirements[course][0] === null
-					) {
-						generateNewSchedule(
-							previousSchedule,
-							majorRequirements,
-							addedClasses
-						);
-					} else {
-						console.log('previousSchedule', previousSchedule);
-						const existingClass = previousSchedule['schedule'][year][
-							quarter
-						].find((c) => c.class === course && c.locked);
-						console.log('existingClass', existingClass);
-						if (existingClass === undefined) {
-							console.log('not locked', course);
-							quarterSchedule.push({ class: course, locked: false }); // Updated to add class as a map
-							addedClasses.push(course);
-						} else {
-							console.log('locked', course);
-							quarterSchedule.push({ class: course, locked: true }); // Updated to add class as a map
-							addedClasses.push(course);
-						}
-					}
-				});
-
-				// Then, add other classes that have satisfied prerequisites
-				Object.keys(majorRequirements.classRequirements)
-					.sort((a, b) => {
-						const prereqsA = majorRequirements.classRequirements[a];
-						const prereqsB = majorRequirements.classRequirements[b];
-						return (
-							(prereqsA ? prereqsA.length : 0) -
-							(prereqsB ? prereqsB.length : 0)
-						);
-					})
-					.forEach((course) => {
-						if (
-							quarterSchedule.length < 2 &&
-							!addedClasses.includes(course) &&
-							majorRequirements.classRequirements[course] !== null &&
-							arePrerequisitesSatisfied(course, majorRequirements, addedClasses)
-						) {
-							const existingClass = previousSchedule['schedule'][year][
-								quarter
-							].find((c) => c.class === course && c.locked);
-							// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
-							// includes
-							console.log('existingClass', existingClass);
-							if (existingClass === undefined) {
-								console.log('not locked', course);
-								quarterSchedule.push({ class: course, locked: false }); // Updated to add class as a map
-								addedClasses.push(course);
-							} else {
-								console.log('locked', course);
-								quarterSchedule.push({ class: course, locked: true }); // Updated to add class as a map
-								addedClasses.push(course);
-							}
-						}
-					});
-
-				// If needed, fill in the remaining slots with random classes
-				while (quarterSchedule.length < 2) {
-					const randomClass = getRandomClass();
-					quarterSchedule.push({ class: randomClass, locked: false }); // Updated to add class as a map
-					addedClasses.push(randomClass);
-				}
-
-				newSchedule[year][quarter] = quarterSchedule;
-			});
-		});
-
-		return newSchedule;
-	};
-
-	const getRandomClass = () => {
-		const randomClasses = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-		return randomClasses[Math.floor(Math.random() * randomClasses.length)];
-	};
-
-	const arePrerequisitesSatisfied = (
-		course,
-		majorRequirements,
-		addedClasses
-	) => {
-		const prerequisites = majorRequirements.classRequirements[course];
-
-		console.log('Checking prerequisites for course:', course);
-		console.log('Prerequisites:', prerequisites);
-
-		if (Array.isArray(prerequisites)) {
-			// Prioritize courses with an empty string value
-			const prioritizedPrerequisites = prerequisites.sort((a, b) => {
-				if (a === null) return -1;
-				if (b === null) return 1;
-				return 0;
-			});
-
-			return prioritizedPrerequisites.every((prereq) =>
-				addedClasses.includes(prereq)
-			);
-		} else {
-			console.error('Prerequisites array is not an array for course:', course);
-			return false;
-		}
-	};
 
 	const renderScheduleTable = () => {
 		const years = Array.from({ length: 4 }, (_, i) => i + 1);
@@ -394,7 +254,9 @@ const Planner = () => {
 			<nav className='navbar'>
 				<h2 className='navbar-title'>{userName}'s Course Planner</h2>
 				<div className='nav-links'>
-					<div className='navbar-buttons' onClick={generateSchedule}>
+					<div
+						className='navbar-buttons'
+						onClick={generateSchedule(currentUser, navigate)}>
 						Generate
 					</div>
 					<div className='navbar-buttons' onClick={signOutHandler} href='/'>
@@ -408,6 +270,14 @@ const Planner = () => {
 };
 
 export default Planner;
+
+export const useUserSchedule = () => {
+	// Initialize userSchedule state with an empty array
+	const [userSchedule, setUserSchedule] = useState([]);
+
+	// Return the state and setter function
+	return { userSchedule, setUserSchedule };
+};
 
 //Component One
 
@@ -442,3 +312,147 @@ const ComponentTwo = () => {
 };
 
 export { ComponentTwo };
+
+const getRandomClass = () => {
+	const randomClasses = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+	return randomClasses[Math.floor(Math.random() * randomClasses.length)];
+};
+
+const arePrerequisitesSatisfied = (course, majorRequirements, addedClasses) => {
+	const prerequisites = majorRequirements.classRequirements[course];
+
+	console.log('Checking prerequisites for course:', course);
+	console.log('Prerequisites:', prerequisites);
+
+	if (Array.isArray(prerequisites)) {
+		// Prioritize courses with an empty string value
+		const prioritizedPrerequisites = prerequisites.sort((a, b) => {
+			if (a === null) return -1;
+			if (b === null) return 1;
+			return 0;
+		});
+
+		return prioritizedPrerequisites.every((prereq) =>
+			addedClasses.includes(prereq)
+		);
+	} else {
+		console.error('Prerequisites array is not an array for course:', course);
+		return false;
+	}
+};
+
+const updateScheduleInFirebase = async (userId, newSchedule) => {
+	try {
+		const db = getFirestore();
+
+		// Reference to the user's schedule document
+		const userScheduleDocRef = doc(db, 'schedules', userId);
+
+		// Use setDoc to either update the existing document or create a new one if it doesn't exist
+		await setDoc(userScheduleDocRef, {
+			_userId: userId,
+			schedule: newSchedule,
+		});
+
+		console.log('Schedule updated in Firebase');
+	} catch (error) {
+		console.error('Error updating schedule in Firebase:', error.message);
+	}
+};
+
+const generateNewSchedule = (
+	previousSchedule,
+	majorRequirements,
+	addedClasses
+) => {
+	// Ensure addedClasses is initialized as an array
+	if (!Array.isArray(addedClasses)) {
+		addedClasses = [];
+	}
+	const newSchedule = {};
+	const years = Array.from({ length: 4 }, (_, i) => i + 1);
+	const quarters = ['fall', 'winter', 'spring', 'summer'];
+	// const allClasses = []; // New array to keep track of all classes
+	years.forEach((year) => {
+		newSchedule[year] = {};
+		quarters.forEach((quarter) => {
+			const quarterSchedule = [];
+			console.log('in generate new schedule.');
+
+			// First, add classes with null value
+			Object.keys(majorRequirements.classRequirements).forEach((course) => {
+				if (
+					quarterSchedule.length < 2 &&
+					!addedClasses.includes(course) &&
+					majorRequirements.classRequirements[course][0] === null
+				) {
+					generateNewSchedule(
+						previousSchedule,
+						majorRequirements,
+						addedClasses
+					);
+				} else {
+					console.log('previousSchedule', previousSchedule);
+					const existingClass = previousSchedule['schedule'][year][
+						quarter
+					].find((c) => c.class === course && c.locked);
+					console.log('existingClass', existingClass);
+					if (existingClass === undefined) {
+						console.log('not locked', course);
+						quarterSchedule.push({ class: course, locked: false }); // Updated to add class as a map
+						addedClasses.push(course);
+					} else {
+						console.log('locked', course);
+						quarterSchedule.push({ class: course, locked: true }); // Updated to add class as a map
+						addedClasses.push(course);
+					}
+				}
+			});
+
+			// Then, add other classes that have satisfied prerequisites
+			Object.keys(majorRequirements.classRequirements)
+				.sort((a, b) => {
+					const prereqsA = majorRequirements.classRequirements[a];
+					const prereqsB = majorRequirements.classRequirements[b];
+					return (
+						(prereqsA ? prereqsA.length : 0) - (prereqsB ? prereqsB.length : 0)
+					);
+				})
+				.forEach((course) => {
+					if (
+						quarterSchedule.length < 2 &&
+						!addedClasses.includes(course) &&
+						majorRequirements.classRequirements[course] !== null &&
+						arePrerequisitesSatisfied(course, majorRequirements, addedClasses)
+					) {
+						const existingClass = previousSchedule['schedule'][year][
+							quarter
+						].find((c) => c.class === course && c.locked);
+						// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+						// includes
+						console.log('existingClass', existingClass);
+						if (existingClass === undefined) {
+							console.log('not locked', course);
+							quarterSchedule.push({ class: course, locked: false }); // Updated to add class as a map
+							addedClasses.push(course);
+						} else {
+							console.log('locked', course);
+							quarterSchedule.push({ class: course, locked: true }); // Updated to add class as a map
+							addedClasses.push(course);
+						}
+					}
+				});
+
+			// If needed, fill in the remaining slots with random classes
+			while (quarterSchedule.length < 2) {
+				const randomClass = getRandomClass();
+				quarterSchedule.push({ class: randomClass, locked: false }); // Updated to add class as a map
+				addedClasses.push(randomClass);
+			}
+
+			newSchedule[year][quarter] = quarterSchedule;
+		});
+	});
+
+	return newSchedule;
+};
